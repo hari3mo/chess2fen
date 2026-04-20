0ac57eaca19adf4cfadda66b1675bb567d9720c0
hari3mp <harisasaif@gmail.com>
2026-04-20 02:08:21 -0700

    crop

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def detect_board(screenshot, debug=False):
         DARK_COLOR_BGR + COLOR_TOLERANCE,
     )
 
-    board_mask = cv2.bitwise_or(light_mask, dark_mask)
+    mask = cv2.bitwise_or(light_mask, dark_mask)
 
     if debug:
-        debug_output(board_mask, 'board_mask_1.png')
+        debug_output(mask, 'board_mask.png')
 
     contours, _ = cv2.findContours(
-        board_mask,
+        mask,
         cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE,
     )
@@ -41,21 +41,30 @@ def detect_board(screenshot, debug=False):
 
     if debug:
         cv2.rectangle(screenshot, (x, y), (x + w, y + h), (0, 0, 255), 3)
-        debug_output(screenshot, 'detected_board_1.png')
+        debug_output(screenshot, 'detected_board.png')
 
     return (x, y, w, h)
+
+def crop_board(screenshot, region, debug=False):
+    x, y, w, h = region
+    w -= w % 8
+    h -= h % 8
+
+    cropped = screenshot[y:y + h, x:x + w]
+
+    if debug:
+        debug_output(cropped, 'cropped_board.png')
+
+    return cropped
     
 def debug_output(image, filename):
     path = os.path.join('./debug', filename)
-    if os.path.exists(path):
-        current_version = os.path.splitext(filename)[0].split('_')[-1]
-        new_version = str(int(current_version) + 1) 
-        filename = filename.replace(current_version, new_version)
-        path = os.path.join('./debug', filename)
     cv2.imwrite(path, image)
+    return path
 
 if __name__ == "__main__":
-    screenshot = load_screenshot('./templates/board.png')
-    board_region = detect_board(screenshot, debug=True)
-    board = ...
-    print("Detected board region (x, y, w, h):", board_region)
+    # screenshot = load_screenshot('./templates/board.png')
+    screenshot = load_screenshot('test.png')
+    region = detect_board(screenshot, debug=True)
+    board = crop_board(screenshot, region, debug=True)
+    print("Detected board region (x, y, w, h):", region)
